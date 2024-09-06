ba50763c8d00f5caa4226bc7b8a8bd02aa49381c
argalloni <alessandro.galloni.11@ucl.ac.uk>
2024-09-06 17:02:59 -0400

    updated readme

--- GLM_regression.py
+++ GLM_regression.py
@@ -568,6 +568,9 @@ def compute_spatial_selectivity_index(avg_residuals):
 
         # Combine the metrics
         spatial_selectivity_index.append(bimodality_coefficient * distance[0])
+
+        spatial_selectivity_index  = a
+
     return spatial_selectivity_index
 
 
